8fbc0fb7886a419f03900cda8dd48fc4398ecac3
Artem Abashev <artem@abashev.com>
2014-07-24 18:00:41 +0400

    Moved unit of work type to the service model.

---
+++ Touch.ServiceModel/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Touch.ServiceModel
+{
+    /// <summary>
+    /// Unit of work.
+    /// </summary>
+    public interface IUnitOfWork : IDisposable
+    {
+        /// <summary>
+        /// Commit operation.
+        /// </summary>
+        /// <exception cref="InvalidOperationException">Unit of work is finished.</exception>
+        void Commit();
+    }
+}

952b203b0ab475e91b965832153f905162b5c766
Artem Abashev <artem@abashev.com>
2014-07-28 23:24:11 +0400

    Touch storage-based cache implementation.

---
+++ ImageResizer.Touch/TouchCache/TouchCache.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections.Specialized;
+using System.Configuration;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Web;
+using ImageResizer.Caching;
+using ImageResizer.Configuration;
+using Touch.Providers;
+using Touch.Storage;
+
+namespace ImageResizer.Plugins.TouchCache
+{
+    public sealed class TouchCache : IPlugin, ICache, IMultiInstancePlugin
+    {
+        #region .ctor
+        public TouchCache(NameValueCollection args)
+        {
+            _args = args;
+        }
+
+        public TouchCache()
+        { }
+        #endregion
+
+        #region Dependencies
+        public static IDependenciesProvider DependenciesProvider { private get; set; }
+
+        public IStorage Storage { private get; set; }
+        #endregion
+
+        #region Data
+        private readonly NameValueCollection _args;
+        #endregion
+
+        #region Public methods
+        public IPlugin Install(Config c)
+        {
+            if (DependenciesProvider == null)
+                throw new ConfigurationErrorsException("Missing DependenciesProvider");
+
+            var plugin = DependenciesProvider.Resolve<TouchCache>();
+
+            //Do the configuration with _args
+
+            c.Plugins.add_plugin(plugin);
+            return this;
+        }
+
+        public bool Uninstall(Config c)
+        {
+            c.Plugins.remove_plugin(this);
+            return true;
+        }
+
+        public bool CanProcess(HttpContext current, IResponseArgs e)
+        {
+            return e.RewrittenQuerystring.AllKeys.Contains("output") && e.RewrittenQuerystring["process"] != ProcessWhen.No.ToString();
+        }
+
+        public void Process(HttpContext current, IResponseArgs e)
+        {
+            var key = e.RewrittenQuerystring["output"];
+
+            if (!Storage.HasFile(key))
+            {
+                var metadata = new Metadata { ContentType = e.RewrittenQuerystring["mime"] };
+
+                using (var data = new MemoryStream())
+                {
+                    e.ResizeImageToStream(data);
+                    Storage.PutFile(data, key, metadata);
+                }
+            }
+
+            if (Storage.IsPublic)
+            {
+                var url = Storage.GetPublicUrl(key);
+                current.Response.Redirect(url, true);
+            }
+            else
+            {
+                Serve(current, e, Storage.GetFile(key));
+            }
+        } 
+        #endregion
+
+        #region Helper methods
+        private static void Serve(HttpContext context, IResponseArgs e, Stream data)
+        {
+            context.RemapHandler(new TouchCacheHandler(e, data));
+        } 
+        #endregion
+    }
+}
---
+++ ImageResizer.Touch/TouchCache/TouchCacheHandler.cs
@@ -0,0 +1,44 @@
+﻿using System.IO;
+using System.Web;
+using ImageResizer.Caching;
+
+namespace ImageResizer.Plugins.TouchCache
+{
+    public sealed class TouchCacheHandler : IHttpHandler
+    {
+        public TouchCacheHandler(IResponseArgs e, Stream data)
+        {
+            _e = e;
+            _data = data;
+        }
+
+        private readonly IResponseArgs _e;
+        private readonly Stream _data;
+        private const int BufferSize = 4096;
+
+        public bool IsReusable
+        {
+            get { return false; }
+        }
+
+        public void ProcessRequest(HttpContext context)
+        {
+            context.Response.StatusCode = 200;
+            context.Response.BufferOutput = false;
+
+            _e.ResponseHeaders.ApplyDuringPreSendRequestHeaders = false;
+            _e.ResponseHeaders.ApplyToResponse(_e.ResponseHeaders, context);
+
+            var buffer = new byte[BufferSize];
+
+            using (_data)
+            {
+                int i;
+                while ((i = _data.Read(buffer, 0, buffer.Length)) > 0)
+                {
+                    context.Response.OutputStream.Write(buffer, 0, i);
+                }
+            }
+        }
+    }
+}

0d3704d6c3fac9f01bf56f2db12cde6e4d6c6521
Artem Abashev <artem@abashev.com>
2014-09-10 16:51:17 +0400

    TouchCache output stream disposal fix.

--- ImageResizer.Touch/TouchCache/TouchCacheHandler.cs
+++ ImageResizer.Touch/TouchCache/TouchCacheHandler.cs
@@ -26,37 +26,37 @@ namespace ImageResizer.Plugins.TouchCache
 
         public void ProcessRequest(HttpContext context)
         {
-            var etag = _e.ResponseHeaders.Headers.AllKeys.Contains("ETag")
-                ? _e.ResponseHeaders.Headers["ETag"]
-                : null;
-            var lastModified = _e.ResponseHeaders.LastModified;
-
-            _e.ResponseHeaders.ApplyDuringPreSendRequestHeaders = false;
-            _e.ResponseHeaders.ApplyToResponse(_e.ResponseHeaders, context);
-
-            context.Response.BufferOutput = false;
-
-            var headers = context.Request.Headers;
-
-            DateTime ifModifiedSince;
-            if (headers.AllKeys.Contains("If-Modified-Since") && DateTime.TryParse(headers["If-Modified-Since"], out ifModifiedSince) && lastModified <= ifModifiedSince)
-            {
-                context.Response.StatusCode = 304;
-                return;
-            }
-
-            if (headers.AllKeys.Contains("If-None-Match") && headers["If-None-Match"] == etag)
-            {
-                context.Response.StatusCode = 304;
-                return;
-            }
-
-            context.Response.StatusCode = 200;
-
-            var buffer = new byte[BufferSize];
-
             using (_data)
             {
+                var etag = _e.ResponseHeaders.Headers.AllKeys.Contains("ETag")
+                    ? _e.ResponseHeaders.Headers["ETag"]
+                    : null;
+                var lastModified = _e.ResponseHeaders.LastModified;
+
+                _e.ResponseHeaders.ApplyDuringPreSendRequestHeaders = false;
+                _e.ResponseHeaders.ApplyToResponse(_e.ResponseHeaders, context);
+
+                context.Response.BufferOutput = false;
+
+                var headers = context.Request.Headers;
+
+                DateTime ifModifiedSince;
+                if (headers.AllKeys.Contains("If-Modified-Since") && DateTime.TryParse(headers["If-Modified-Since"], out ifModifiedSince) && lastModified <= ifModifiedSince)
+                {
+                    context.Response.StatusCode = 304;
+                    return;
+                }
+
+                if (headers.AllKeys.Contains("If-None-Match") && headers["If-None-Match"] == etag)
+                {
+                    context.Response.StatusCode = 304;
+                    return;
+                }
+
+                context.Response.StatusCode = 200;
+
+                var buffer = new byte[BufferSize];
+
                 int i;
                 while ((i = _data.Read(buffer, 0, buffer.Length)) > 0)
                 {
